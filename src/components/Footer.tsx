4ad2b484973680856122d32d0e11fc8ea5b7de4c
IagoMig1 <iago.miguel3008@outlook.com>
2025-04-14 14:03:20 -0300

    atualização

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,6 +7,6 @@ export function Footer() {
         <p className="text-pink-700">Iago & Julia</p>
         <HeartIcon className="h-5 w-5 text-red-500" />
       </div>
-      <p className="text-gray-600 text-sm">Juntos desde 11/11/2023</p>
+      <p className="text-gray-600 text-sm">Juntos desde 11/12/2023</p>
     </footer>;
 }
